d46375d06b0016e7b9b9c2fe96ba9621c1830aef
reaugustofer <renan.ferreira@numenit.com>
2024-02-05 12:02:28 -0300

    node modularizado e chamadas estruturadas

--- srv/ias-service.js
+++ srv/ias-service.js
@@ -4,6 +4,7 @@ module.exports = (srv) => {
 
     srv.on('handleDuplicatedUsers', async req => {
 
+        // -----> get ias user to be processed
         try {
             let usersIAS = await executeHttpRequest(
                 {
@@ -22,143 +23,12 @@ module.exports = (srv) => {
             );
 
             let oResources = usersIAS.data.Resources;
-            let aUsersToBeChk = getUsersToCheck(oResources);
-            let aUsersCustomId = buildUsersCustomId(aUsersToBeChk);
-            let aUsersToBeUpd = buildUsersToBeUpd(aUsersCustomId, aUsersToBeChk);
-            return aUsersToBeUpd;
+            let aUsersToUpdate = handleDuplicatedUsers(oResources);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            /* let lt_proc_users = [];
-            let lt_proc_users_upd = [];
-            let lt_user_id = [];
-
-            data.Resources.forEach(element => {
-
-                var attributs = element.urnSapCloudScimSchemasExtensionCustom20User.attributes;
-                var lv_atr1 = '', lv_atr2 = '', lv_atr3 = '', lv_atr4 = '';
-                attributs.forEach(attr => {
-                    switch (attr._name) {
-                        case 'customAttribute1':
-                            lv_atr1 = attr.value;
-                            break;
-                        case 'customAttribute2':
-                            lv_atr2 = attr.value;
-                            break;
-                        case 'customAttribute3':
-                            lv_atr3 = attr.value;
-                            break;
-                        case 'customAttribute4':
-                            lv_atr4 = attr.value;
-                            break;
-                    }
-                });
-
-                lt_proc_users.push({
-                    id: element.id,
-                    email: element.emails[0].value,
-                    atr1: lv_atr1,
-                    atr2: lv_atr2,
-                    atr3: lv_atr3,
-                    atr4: lv_atr4,
-                });
-
-                var lv_user_id = lv_atr1 + lv_atr2;
-                var lv_count = 0;
-
-                var lo_user_id = lt_user_id.find(o => o.login === lv_user_id);
-                if (lo_user_id) {
-
-                    lo_user_id.count += 1;
-                    lo_user_id.lt_user.push({
-                        id: element.id,
-                        email: element.emails[0].value,
-                        created: element.meta.created,
-                        atr1: lv_atr1,
-                        atr2: lv_atr2,
-                        atr3: lv_atr3,
-                        atr4: lv_atr4,
-                    });
-                    lo_user_id.lt_created.push(element.meta.created);
-
-                } else {
-                    lo_user_id = {
-                        login: lv_user_id,
-                        count: 0,
-                        lt_user: [],
-                        lt_created: [],
-                    };
-                    lo_user_id.lt_user.push({
-                        id: element.id,
-                        email: element.emails[0].value,
-                        created: element.meta.created,
-                        atr1: lv_atr1,
-                        atr2: lv_atr2,
-                        atr3: lv_atr3,
-                        atr4: lv_atr4,
-                    });
-                    lo_user_id.lt_created.push(element.meta.created);
-                    lt_user_id.push(lo_user_id);
-                }
-
-            });
-
-            let ls_user_id;
-
-            lt_user_id.forEach(ls_user_id => {
-
-                ls_user_id.lt_created.sort();
-                let lt_dup = ls_user_id.lt_user;
-                var lv_nuser = -1;
-
-                for (let i = 0; i < ls_user_id.lt_created.length; i++) {
-                    for (let j = 0; j < lt_dup.length; j++) {
-
-                        var ls_dup = lt_dup[j];
-
-                        if (ls_user_id.lt_created[i] === ls_dup.created) {
-
-                            lv_nuser += 1;
-                            if (ls_dup.atr3 === '') {
-                                ls_dup.atr3 = ls_user_id.login;
-                            }
-
-                            if (ls_dup.atr4 === '') {
-                                var lv_nc = lv_nuser.toString();
-                                ls_dup.atr4 = lv_nc;
-                                if (lv_nuser > 0) {
-                                    if ((ls_user_id.login.length + lv_nc.length) <= 12) {
-                                        ls_dup.atr3 = ls_user_id.login + lv_nc;
-                                    } else {
-                                        ls_dup.atr3 = ls_user_id.login.slice(0, ls_user_id.login.length - lv_nc.length) + lv_nc;
-                                    }
-                                }
-                                lt_proc_users_upd.push(ls_dup);
-                            }
-                        }
-                    }
-                }
-            });
-
-            lt_proc_users_upd.forEach(async ls_user_id => {
+            // -----> update ias user with new customAttribute values
+            aUsersToUpdate.forEach(async userUpd => {
                 try {
-                    let response2 = await executeHttpRequest(
+                    let updUserIAS = await executeHttpRequest(
                         {
                             //destinationName: "NumenIAS",
                             url: "https://a2ktkozpw.accounts.ondemand.com/scim",
@@ -167,11 +37,10 @@ module.exports = (srv) => {
                         },
                         {
                             method: "patch",
-                            url: "/Users/" + ls_user_id.id,
+                            url: "/Users/" + userUpd.id,
                             headers: {
                                 "Content-Type": "application/scim+json; charset=utf-8"
                             },
-                            // BODY
                             data: {
                                 "schemas": ["urn:ietf:params:scim:api:messages:2.0:PatchOp"],
                                 "Operations": [
@@ -180,12 +49,12 @@ module.exports = (srv) => {
                                         "path": "urn:sap:cloud:scim:schemas:extension:custom:2.0:User:attributes",
                                         "value": [
                                             {
-                                                "name": "customAttribute4",
-                                                "value": ls_user_id.atr4
+                                                "name": "customAttribute3",
+                                                "value": userUpd.customAttr3
                                             },
                                             {
-                                                "name": "customAttribute3",
-                                                "value": ls_user_id.atr3
+                                                "name": "customAttribute4",
+                                                "value": userUpd.customAttr4
                                             }
                                         ]
                                     }
@@ -194,16 +63,23 @@ module.exports = (srv) => {
                         }
                     );
                 } catch (error) {
+                    console.log('error on api call (patch), id:', userUpd.id);
                     console.log(error);
                 }
-            }); */
+            });
 
         } catch (error) {
             console.log('error on api call (get)');
             console.log(error);
         }
+    })
+
+    function handleDuplicatedUsers(UsersIAS) {
+        let aUsersToBeChk = getUsersToCheck(UsersIAS);
+        let aUsersCustomId = buildUsersCustomId(aUsersToBeChk);
+        let aUsersToBeUpd = buildUsersToBeUpd(aUsersCustomId);
+        return aUsersToBeUpd;
     }
-    )
 
     function getUsersToCheck(UsersIAS) {
         let aUsers = [];
@@ -241,14 +117,25 @@ module.exports = (srv) => {
         return aUsers;
     }
 
-    function buildUsersToBeUpd(UsersToBeUpd, CurrentUsers) {
+    function buildUsersToBeUpd(UsersToBeUpd) {
         let aMainListIds = removeDuplicateIds(UsersToBeUpd);
-        return aMainListIds;
+        let aUsersToBeUpd = UsersToBeUpd;
+        aMainListIds.forEach(id => {
+            let iCont = 0;
+            aUsersToBeUpd.forEach(user => {
+                if (user.customAttr3 == id) {
+                    user.customAttr3 += iCont == 0 ? "" : iCont;
+                    user.customAttr4 = iCont;
+                    iCont++;
+                }
+            })
+        })
+        return aUsersToBeUpd;
     }
 
     function removeDuplicateIds(IdList) {
-        // make a especific list with customattr3 field, then using this in filter operator
-        return IdList.filter((list, index) => IdList.indexOf(list.customAttr3) === index);
+        let aFilteredList = IdList.map(list => list.customAttr3);
+        return aFilteredList.filter((list, index) => aFilteredList.indexOf(list) === index);
     }
 
 }
